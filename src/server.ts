5d5ddee7b12958af32472de854bd727a1ef2b8cf
ooliveiratg <menezesth16@gmail.com>
2025-06-28 13:40:40 -0300

    feat(database): connection established between database and organizations files

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,20 @@
-import fastify from "fastify";
+/* eslint-disable prettier/prettier */
+import fastify from 'fastify'
+import { knex } from './database'
 
 const server = fastify()
 const port = 7777
 
-server.listen({
-    port: port
-}, () => console.log('Server is running on port ' + port))
+server.get('/hello', async () => {
+  const tables = await knex('sqlite_schema').select('*')
+
+  return tables
+})
+
+server.listen(
+  {
+    port,
+  },
+  
+  () => console.log('Server is running on port ' + port),
+)
